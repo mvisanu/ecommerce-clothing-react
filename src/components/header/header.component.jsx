0b3ca2bb7420312d91ca458f43b53fa4f29f0720
mvisanu@gmail.com <mvisanu@gmail.com>
2022-02-19 11:48:44 -0600

    updating our app to redicrect to home if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,3 +1,4 @@
+   
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -8,26 +9,27 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser}) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo"></Logo>
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    <div className='options'>
+      <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className="option" to="/shop">
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
-      {
-        currentUser ?
-        <div className='option' onClick={() => auth.signOut()}> SIGN OUT</div>
-        :
-        <Link className="option" to="/signin">
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
           SIGN IN
         </Link>
-      }
+      )}
     </div>
   </div>
 );
