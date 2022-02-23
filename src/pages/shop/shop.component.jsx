8dc29d75df42cf610624c111600669c2001cce0f
mvisanu@gmail.com <mvisanu@gmail.com>
2022-02-23 07:57:55 -0600

    Add directory and shop into redux reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import SHOP_DATA from './shop.data.js'
+import { selectCollections } from '../../redux/shop/shop.selectors';
+
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ collections }) => (
 
-        this.state = {
-            collections: SHOP_DATA
-        };
-   }
+  <div className='shop-page'>
+        {
+            collections.map(({id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps}>
+                </CollectionPreview>
+            ))
+        }
+    </div>);
+   
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-    render() {
-        const { collections } = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}>
-                    </CollectionPreview>
-                ))
-            }
-        </div>);
-    }
-
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
