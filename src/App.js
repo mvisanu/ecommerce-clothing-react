afe3a932d71cc37fa5ece814021404d19b3fee0c
mvisanu@gmail.com <mvisanu@gmail.com>
2022-02-25 17:18:37 -0600

    Add Firestore and Spinner Components

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from './redux/user/user.selectors';
 
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -34,6 +36,10 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+    //   addCollectionAndDocuments(
+    //     'collections',
+    //     collectionsArray.map(({ title, items }) => ({ title, items })));
+      
     });
   }
 
@@ -67,7 +73,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector ({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = (dispatch) => ({
