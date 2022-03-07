15cd8e58b506265e4eb4aebf2cd110f18eff7d2d
Visanu <mvisanu@gmail.com>
2022-03-07 15:25:03 -0600

    Latest saga

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from "./pages/checkout/checkout.component";
 
 import Header from "./components/header/header.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+//import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -21,28 +21,28 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    //const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    //   if (userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(
+    //     userRef.onSnapshot(
           
-          (snapShot) => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data(),
-          });
-        });
-      }
+    //       (snapShot) => {
+    //       setCurrentUser({
+    //         id: snapShot.id,
+    //         ...snapShot.data(),
+    //       });
+    //     });
+    //   }
 
-      setCurrentUser(userAuth);
+    //   setCurrentUser(userAuth);
     //   addCollectionAndDocuments(
     //     'collections',
     //     collectionsArray.map(({ title, items }) => ({ title, items })));
       
-    });
+    //});
   }
 
   componentWillUnmount() {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
-import { persistStore } from 'redux-persist';
+//import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 
 import rootReducer from "./root-reducer";
+import rootSaga from "./root-saga";
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
@@ -14,5 +17,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store);
+//export const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
 
